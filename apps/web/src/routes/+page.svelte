cd86fe1de0636f0bfc5295b7d53c1953a33b6c18
Ben <49216935+ben-bosmans@users.noreply.github.com>
2025-07-27 00:43:33 +1000

    feat: add code editor component

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import CodeEditor from '$lib/components/CodeEditor.svelte';
+	let value = $state('console.log("Hello World!");');
+
+	$effect(() => {
+		console.log(value);
+		console.log('something else');
+		console.log('again');
+	});
+</script>
+
+<CodeEditor bind:value language="typescript" />
