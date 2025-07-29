b0390c464e68d8b7fa07c4256a040fcf3e898af2
Ben <49216935+ben-bosmans@users.noreply.github.com>
2025-07-30 00:38:39 +1000

    feat: add session creation to auth service

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
-@Module({})
+@Module({
+  imports: [JwtModule],
+  providers: [AuthService],
+  controllers: [AuthController],
+})
 export class AuthModule {}
--- apps/api/src/modules/auth/auth.service.ts
+++ apps/api/src/modules/auth/auth.service.ts
@@ -1,10 +1,17 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/modules/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { Config } from 'src/common/config/schema.config';
 import * as argon2 from 'argon2';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+    private configService: ConfigService<Config>,
+  ) {}
 
   async signUp(email: string, password: string) {
     const user = await this.prisma.user.findFirst({ where: { email } });
@@ -16,8 +23,43 @@ export class AuthService {
     const hashedPassword = await argon2.hash(password, {
       type: argon2.argon2id,
     });
-    await this.prisma.user.create({
+    const newUser = await this.prisma.user.create({
       data: { email, password: hashedPassword },
     });
+
+    const tokens = await this.getTokens(newUser.id, email);
+    await this.createSession(newUser.id, tokens.refreshToken);
+
+    return tokens;
+  }
+
+  private async createSession(userId: string, refreshToken: string) {
+    const refreshTokenHash = await argon2.hash(refreshToken);
+    await this.prisma.session.create({
+      data: {
+        user: { connect: { id: userId } },
+        refreshTokenHash,
+      },
+    });
+  }
+
+  private async getTokens(userId: string, email: string) {
+    const payload = { sub: userId, email };
+
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwtService.signAsync(payload, {
+        secret: this.configService.get('JWT_ACCESS_SECRET'),
+        expiresIn: '15m',
+      }),
+      this.jwtService.signAsync(payload, {
+        secret: this.configService.get('JWT_REFRESH_SECRET'),
+        expiresIn: '7d',
+      }),
+    ]);
+
+    return {
+      accessToken,
+      refreshToken,
+    };
   }
 }
