176f644a66d1939b64be2afb6722a717780cd8e8
Ben <49216935+ben-bosmans@users.noreply.github.com>
2025-07-29 21:18:20 +1000

    feat: add sign up dto

---
+++ packages/dto/src/auth/signUp.dto.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const signUpSchema = z.object({
+  email: z.email(),
+  password: z.string().min(8).max(64),
+});
+
+export type SignUpDto = z.infer<typeof signUpSchema>;
---
+++ packages/dto/src/index.ts
@@ -0,0 +1,1 @@
+export * from "./auth/signUp.dto";
