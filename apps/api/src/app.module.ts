b05d398881dd3728f06c3817eae494cb33588c07
Ben <49216935+ben-bosmans@users.noreply.github.com>
2025-07-30 00:07:42 +1000

    feat: add config module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { validateConfig } from './common/config/schema.config';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      validate: validateConfig,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ apps/api/src/common/config/schema.config.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+const configSchema = z.object({
+  DATABASE_URL: z.string().min(1),
+  JWT_ACCESS_SECRET: z.string().min(1),
+  JWT_REFRESH_SECRET: z.string().min(1),
+});
+
+export const validateConfig = (config: Record<string, unknown>) => {
+  return z.parse(configSchema, config);
+};
+
+export type Config = z.infer<typeof configSchema>;
