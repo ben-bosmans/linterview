d8ba991d2082eea73f824caa5df21944266e2bdc
Ben <49216935+ben-bosmans@users.noreply.github.com>
2025-08-02 13:08:53 +1000

    feat: add refresh token cleanup service

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,11 +5,13 @@ import { validateConfig } from './common/config/schema.config';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ZodSerializerInterceptor } from './common/interceptors/zod-serializer.interceptor';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     AuthModule,
     PrismaModule,
+    ScheduleModule.forRoot(),
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
