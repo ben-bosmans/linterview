31e4ee7ad55ef61f965c963ac9344c27f044a9cf
Ben <49216935+ben-bosmans@users.noreply.github.com>
2025-08-02 11:16:00 +1000

    feat: add refresh tokens method

--- apps/api/src/modules/auth/auth.service.ts
+++ apps/api/src/modules/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/modules/prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
@@ -7,7 +11,7 @@ import * as argon2 from 'argon2';
 import {
   ACCESS_TOKEN_EXPIRES,
   REFRESH_TOKEN_EXPIRES_DAYS,
-} from './constants/auth.constants';
+} from './auth.constants';
 import { randomBytes, createHash } from 'crypto';
 import { addDays } from 'date-fns';
 
@@ -23,7 +27,7 @@ export class AuthService {
     const user = await this.prisma.user.findFirst({ where: { email } });
 
     if (user) {
-      throw new BadRequestException('A user with this email already exists');
+      throw new BadRequestException('Unable to create account');
     }
 
     const hashedPassword = await argon2.hash(password, {
@@ -39,14 +43,41 @@ export class AuthService {
   async signIn(email: string, password: string) {
     const user = await this.prisma.user.findFirst({ where: { email } });
 
-    if (!user) throw new BadRequestException('Invalid credentials');
+    if (!user) throw new UnauthorizedException('Invalid credentials');
 
     if (await argon2.verify(user.password, password)) {
       return await this.getTokens(user.id, email);
-    } else throw new BadRequestException('Invalid credentials');
+    } else throw new UnauthorizedException('Invalid credentials');
   }
 
-  async refreshTokens(refreshToken: string) {}
+  async refreshTokens(refreshToken: string) {
+    const tokenHash = this.hashRefreshToken(refreshToken);
+
+    console.log(tokenHash);
+
+    const refreshTokenRecord = await this.prisma.refreshToken.findFirst({
+      where: { tokenHash },
+      include: { user: true },
+    });
+
+    console.log(refreshTokenRecord);
+
+    if (!refreshTokenRecord) {
+      throw new UnauthorizedException('Session expired');
+    }
+
+    await this.prisma.refreshToken.delete({
+      where: { id: refreshTokenRecord.id },
+    });
+
+    if (refreshTokenRecord.expiresAt < new Date())
+      throw new UnauthorizedException('Session expired');
+
+    return await this.getTokens(
+      refreshTokenRecord.user.id,
+      refreshTokenRecord.user.email,
+    );
+  }
 
   private async getTokens(userId: string, email: string) {
     const [accessToken, refreshToken] = await Promise.all([
@@ -73,7 +104,7 @@ export class AuthService {
 
   private async getRefreshToken(userId: string) {
     const refreshToken = randomBytes(64).toString('hex');
-    const tokenHash = createHash('sha256').update(refreshToken).digest('hex');
+    const tokenHash = this.hashRefreshToken(refreshToken);
     const expiresAt = addDays(new Date(), REFRESH_TOKEN_EXPIRES_DAYS);
 
     await this.prisma.refreshToken.create({
@@ -85,5 +116,9 @@ export class AuthService {
     });
 
     return refreshToken;
+  }
+
+  private hashRefreshToken(refreshToken: string) {
+    return createHash('sha256').update(refreshToken).digest('hex');
   }
 }
