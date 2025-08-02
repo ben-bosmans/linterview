bdff69d574c1c09887ffcfe4fbaba5ae84221d76
Ben <49216935+ben-bosmans@users.noreply.github.com>
2025-08-02 11:15:46 +1000

    feat: add auth guard and public decorator

---
+++ apps/api/src/modules/auth/auth.guard.ts
@@ -0,0 +1,53 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { Config } from 'src/common/config/schema.config';
+import { IS_PUBLIC_KEY } from './public.decorator';
+import { JwtPayload } from './auth.types';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private jwtService: JwtService,
+    private configService: ConfigService<Config>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    const request: Request = context.switchToHttp().getRequest();
+
+    const token = this.extractTokenFromHeader(request);
+    if (!token && !isPublic) throw new UnauthorizedException();
+
+    if (token) {
+      try {
+        const payload = await this.jwtService.verifyAsync<JwtPayload>(token, {
+          secret: this.configService.get('JWT_ACCESS_SECRET'),
+        });
+        console.log(payload);
+        request.user = payload;
+      } catch {
+        if (!isPublic) throw new UnauthorizedException();
+      }
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
